0433193d7fde77c2bedbaa90fda9135a0919ed1f
Gallinaccio02 <>
2022-04-08 10:41:47 +0000

    Initial commit

---
+++ Client.java
@@ -0,0 +1,5 @@
+public class Client {
+
+Socket socket = new Socket(“127.0.0.1”,2000);
+  close();
+}
---
+++ Server.java
@@ -0,0 +1,16 @@
+public class Server{
+  ServerSocket serverSocket=new ServerSocket(2000);
+  Socket socket=serverSocket.accept();
+  close();
+
+    import java.io.BufferedWriter
+  public class BufferedWriter{
+  
+    write("Ciao")
+ 
+  
+  }
+    
+
+}
+
